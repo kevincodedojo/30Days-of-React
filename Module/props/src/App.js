491466b87539bb2cb2daa16c2e9c604eaf65a6ef
“kevin” <ashen7@mail.ccsf.edu>
2024-03-29 13:58:27 -0700

    day 10 update

---
+++ Module/props/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// export const App = () => <h1>Welcome to 30 Days of React</h1>;
+
+//---------------------------------or export in a funciton declaration
+// export function App() {
+//   return <h1>Welcome to 30 Days of React</h1>;
+// }
+
+//-----default export
+const App = () => <h1>Welcome to 30 Days of React</h1>;
+export default App;
